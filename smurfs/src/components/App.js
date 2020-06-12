cd16837ed6a0d1b72d4457923e38a0577131bda2
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-12 12:07:19 -0400

    smurf renders

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,32 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { SmurfContext } from '../contexts/SmurfContext.js'
+
+import axios from 'axios'
 import "./App.css";
-class App extends Component {
-  render() {
+
+import Smurfs from './Smurfs'
+
+function App() {
+
+  const [ smurfs, setSmurfs ] = useState([]);
+
+  useEffect(() => { 
+    axios.get(`http://localhost:3333/smurfs`)
+      .then(res => {
+        console.log(res)
+        setSmurfs(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }, [])
+
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+    <SmurfContext.Provider value={{smurfs}}>
+        <Smurfs/>
+    </SmurfContext.Provider>
+    )
+
 }
 
 export default App;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { SmurfContext } from '../contexts/SmurfContext'
+
+import Smurf from './Smurf'
+
+const Smurfs = () => {
+
+    const { smurfs } = useContext(SmurfContext)
+
+    return (
+        <div className='smurf-village'>
+            <h2>My Smurf Village</h2>
+            {
+                smurfs.map(item => (
+                    <Smurf smurf={item}/>
+                ))
+
+            }
+        </div>
+    )
+}
+
+export default Smurfs
